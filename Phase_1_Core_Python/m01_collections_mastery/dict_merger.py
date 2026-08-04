95df1aa97d54bbcd645aeddad18ec35a0575c629
Sameer G Herkal <sameergherkal@192.168.1.6>
2026-08-04 23:37:31 +0530

    2 problems solved

--- Phase_1_Core_Python/m01_collections_mastery/dict_merger.py
+++ Phase_1_Core_Python/m01_collections_mastery/dict_merger.py
@@ -3,3 +3,33 @@ Milestone: m01_collections_mastery
 Task: Dict Merger
 """
 
+# The Scenario
+# You are building an AI application settings manager. You have a dictionary containing system defaults and 
+# another containing user preferences. You need to merge them so that the user's 
+# choices overwrite the defaults, and any new unique user settings are added.
+
+##Inputs
+default_config = {
+    "model": "gpt-4o",
+    "temperature": 0.7,
+    "max_tokens": 1024,
+    "stream": False
+}
+
+user_config = {
+    "temperature": 0.2,
+    "max_tokens": 2048,
+    "presence_penalty": 0.5
+}
+
+
+# {
+#     "model": "gpt-4o",
+#     "temperature": 0.2,
+#     "max_tokens": 2048,
+#     "stream": False,
+#     "presence_penalty": 0.5
+# }
+
+new_dict = default_config | user_config
+print(new_dict)
--- Phase_1_Core_Python/m01_collections_mastery/duplicate_prompts.py
+++ Phase_1_Core_Python/m01_collections_mastery/duplicate_prompts.py
@@ -2,4 +2,33 @@
 Milestone: m01_collections_mastery
 Task: Duplicate Prompts
 """
+raw_prompts = [
+    "Write a quicksort algorithm in Python",
+    "Explain quantum computing simply",
+    "Write a quicksort algorithm in Python",  # Duplicate
+    "What is the capital of France?",
+    "Explain quantum computing simply",        # Duplicate
+    "Write a quicksort algorithm in Python"   # Duplicate
+]
 
+# Expected Final Output:
+# {
+#     "Write a quicksort algorithm in Python": 3,
+#     "Explain quantum computing simply": 2
+# }
+
+prompts_count = {}
+for prompt in raw_prompts:
+    if prompt in prompts_count:
+        prompts_count[prompt] +=1
+    else:
+        prompts_count[prompt] =1
+        
+print(prompts_count)
+
+duplicate_res = {}
+
+for prompt, count in prompts_count.items():
+    if count>1:
+        duplicate_res[prompt] = count
+print(duplicate_res)
